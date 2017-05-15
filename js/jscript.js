8155c825b08cd4b5679d22c7e3e95fd5ac503e84
Abdel <alexb5985@gmail.com>
2017-05-15 08:12:04 +0000

    Add files via upload

---
+++ js/jscript.js
@@ -0,0 +1,34 @@
+
+function ToggleMenu() {
+   var x = document.querySelector('nav ul');
+   x.classList.toggle("toggle_menu");
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+/*  
+function printTime() {
+  var d = new Date();
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  document.body.innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
+ 
+  */
+
+  
+  
+  /* Github */
